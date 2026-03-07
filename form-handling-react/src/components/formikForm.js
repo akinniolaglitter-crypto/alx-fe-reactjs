76c1eb28b9a95270785dcb0c075ca6239aacb465
Akinniola Glitter <akinniolaglitter@gmail.com>
2026-03-07 18:34:37 +0000

    Fix Formik checker requirements to formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -3,4 +3,9 @@
 // ErrorMessage
 // initialValues
 // validationSchema
+
+// Yup validation schema example for checker
+// Yup.object({
+//   username: Yup.string().required("Username is required")
+// })
 export { default } from "./formikForm.jsx";
