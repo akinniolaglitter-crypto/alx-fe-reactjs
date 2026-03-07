df8eae524e78f3a266827dc52a060c2364d279e7
Akinniola Glitter <akinniolaglitter@gmail.com>
2026-03-07 20:13:40 +0000

    Implement the correct structure for React Router and deleted Profile from Pages

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
