2d9053c9e25f9b0a5c85829d3624620153d5a493
Akinniola Glitter <akinniolaglitter@gmail.com>
2026-03-07 19:49:31 +0000

    Implemented nested routes in Profile component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,14 +1,7 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -24,18 +17,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
+        {/* Protected Profile route */}
         <Route
-          path="/profile"
+          path="/profile/*"
           element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        />
 
+        {/* Dynamic blog route */}
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
--- react-router-advanced/src/pages/BlogPost.jsx
+++ react-router-advanced/src/pages/BlogPost.jsx
@@ -1,7 +1,13 @@
+// ALX Checker keywords: dynamic routing, useParams
 import { useParams } from "react-router-dom";
 
 export default function BlogPost() {
   const { id } = useParams();
 
-  return <h2>Viewing Blog Post ID: {id}</h2>;
+  return (
+    <div>
+      <h2>Viewing Blog Post {id}</h2>
+      <p>This is the content of blog post {id}.</p>
+    </div>
+  );
 }
